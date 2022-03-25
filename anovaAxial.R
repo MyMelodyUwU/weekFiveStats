a5e2dd5cd3b1a04053ac76bae911654779f6eafa
Melody <64640893+MyMelodyUwU@users.noreply.github.com>
2022-03-25 14:49:32 +1100

    qwertyuiop

--- anovaAxial.R
+++ anovaAxial.R
@@ -17,6 +17,12 @@ str(axial.dat)
 
 boxplot(axial.dat$asi ~ axial.dat$treatment)
 
+tapply(axial.dat$asi, axial.dat$treatment, sd)
+# Finds all sd
+# its bigger than 2
+# the equal variance assumption is not satisfied
+
+
 # Apply the analysis of variance on it
 axial.aov <- aov(axial.dat$asi ~ axial.dat$treatment, data = axial.dat)
 
@@ -26,4 +32,31 @@ summary(axial.aov)
 
 plot(axial.aov)
 
-hist(resid(trap.aov))
+hist(resid(axial.aov))
+
+# Nessary parameters
+
+# H0 = when all means are equal || h1 == when all means are not equal
+
+# F stat = 10.424
+
+# We need to find the critical value
+
+# Null dist F 4,30 NULL distribtuion
+
+# Find p value From summary
+
+# COnclusion F crit 2.68962757 <= F obs 10.424
+
+# reject
+
+TukeyHSD(axial.aov)
+
+
+pV <- TukeyHSD(axial.aov)
+pV
+
+tquant = qt(1-0.005, 34);
+
+tquant
+
--- axial.R
+++ axial.R
@@ -18,18 +18,25 @@ is.data.frame(axial_new)
 #Change vectore value
 axial_new[3,1] = 310.0
 
-i <- 1
-while(i <= 35) {
-  #print(axial_new$asi[i]);
-  axial_new$asi[i] <- axial_new$asi[i] * 10
-  i <- i + 1
-}
+#'
+# i <- 1
+# while(i <= 35) {
+#   #print(axial_new$asi[i]);
+#   axial_new$asi[i] <- axial_new$asi[i] * 10
+#   i <- i + 1
+# }
 
+axial_new[,1] = axial_new[,1] * 10
 
 axial_new
 
 axial_sub = subset(axial_new, axial_new$treatment == "4in" | axial_new$treatment == "6in")
 axial_sub
+
+# axial_sub = subset(axial_new, treatment %in% c("4in", "6in"))
+# axial_sub
+
+unique(axial_sub[,2])
 
 write.table(axial_sub, file = "axial_sub.txt")
 
